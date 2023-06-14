d912e9f60b2584a0cc52a4e5df8f137133cebb3f
Kihoon Lee <kihooncs@gmail.com>
2023-06-14 01:31:53 -0700

    First commit

---
+++ Scrumdinger2/Scrumdinger2App.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger2App.swift
+//  Scrumdinger2
+//
+//  Created by Kihoon Lee on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger2App: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
